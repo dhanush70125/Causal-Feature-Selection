3a30211421a15dcd0358fda1d106f5eb2d032384
dhanush70125 <83578486+dhanush70125@users.noreply.github.com>
2021-05-04 12:25:19 +0200

    Add files via upload

---
+++ clean data(KNN).R
@@ -0,0 +1,74 @@
+#importing dataset
+clean_data = read.csv("D:/RT/Basecamp/CleanedDataset_SyS.csv")
+selected_data = clean_data[-c(4:8,10:13)]
+
+library(tidyverse)
+library(dummies)
+
+selected_data <- dummies::dummy.data.frame(
+  as.data.frame(selected_data),
+  names = c("localisation"),
+  sep = "="
+) %>%
+  as_tibble()
+
+#encoding categorical data
+selected_data$RuptureState = as.factor(selected_data$RuptureState)
+selected_data$MultipleAneurysm = as.factor(selected_data$MultipleAneurysm)
+str(selected_data)
+
+#splitting data
+library(caTools)
+set.seed(123)
+split = sample.split(selected_data$RuptureState, SplitRatio = 0.7)
+train_set = as.data.frame(subset(selected_data, split == TRUE))
+test_set = as.data.frame(subset(selected_data, split == FALSE))
+
+#feature scaling
+train_set[,14] = scale(train_set[,14])
+test_set[,14] = scale(test_set[,14])
+
+#fitting KNN on train_set and also predicting the test_set results
+library(class)
+y_pred = knn(train = train_set[, -1],
+             test = test_set[, -1],
+             cl = train_set[, 1],
+             k = 5)
+
+#making confusion matrix
+cm = table(test_set[,1], y_pred)
+
+#Applying k-fold cross validation
+#install.packages('caret')
+library(caret)
+folds = createFolds(train_set$RuptureState, k = 10) #class list of 10 accuracies
+cv = lapply(folds, function(x) {
+  train_fold = train_set[-x,]
+  test_fold = train_set[x,]
+  y_pred = knn(train = train_fold[, -1],
+               test = test_fold[, -1],
+               cl = train_fold[, 1],
+               k = 5)
+  cm = table(test_fold[,1], y_pred)
+  accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[1,1] + cm[1,2] + cm[2,1])
+  return(accuracy)
+})
+mean_accuracy = mean(as.numeric(cv))
+
+# Applying Grid Search to find the best parameters
+# install.packages('caret')
+library(caret)
+grid_tree = train(form = RuptureState ~ ., 
+                  data = train_set, method = 'knn')
+
+tuned_parameters = grid_tree$bestTune
+
+#fitting KNN on train_set including tuned parameters and also predicting the test_set results
+library(class)
+y_pred_tuned = knn(train = train_set[, -1],
+                   test = test_set[, -1],
+                   cl = train_set[, 1],
+                   k = 9)
+
+#making confusion matrix
+cm_tuned = table(test_set[,1], y_pred_tuned)
